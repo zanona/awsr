30a9a3c2554df3553a48736ad12057be2db1835c
Marcus Zanona <zanona@me.com>
2016-11-04 18:33:24 +0000

    Add support for lambda aliases

--- cli.js
+++ cli.js
@@ -3,39 +3,47 @@
 var program = require('commander'),
     stdin = '';
 
-program.version(require('../package.json').version);
+program.version(require('./package.json').version);
 program
     .command('apigateway:push <definition>')
     .description('Push API Gateway providing a Swagger definition')
     .option('-i --id <id>', 'The API ID')
     .action(function (definition, options) {
-        require('../lib/apigateway').push(options.id, definition);
+        require('./lib/apigateway').push(options.id, definition);
     });
 program
     .command('lambda:push [file]')
     .description('Push JS or ZIP file with AWS Lambda.')
     .description('Accepts base64 zip file as STDIN as well')
     .option('-f --fn <function-name>', 'Lambda Function Name')
+    .option('-a --alias [alias]', 'Version Alias')
     .action(function (filePath, options) {
-        var fileData;
-        if (stdin) { fileData = stdin; }
-        if (!filePath && !fileData) {
-            return console.error('Missing zip file');
-        }
-        require('../lib/lambda').push(filePath, fileData, options.fn);
+      var fileData;
+      if (stdin) fileData = stdin;
+      if (!filePath && !fileData) return console.error('Missing zip file');
+      const alias = typeof options.alias === 'string' ? options.alias : void 0;
+      require('./lib/lambda').push(filePath, fileData, options.fn, alias);
+    });
+program
+    .command('lambda:alias <alias>')
+    .description('Alias latest version of a lambda function')
+    .option('-f --fn <function-name>', 'Lambda Function Name')
+    .option('-d --delete', 'Should delete alias')
+    .action(function (alias, options) {
+      require('./lib/lambda').alias(options.fn, alias, options.delete);
     });
 program
     .command('lambda:approve <arn>')
     .description('Approve lambda invocation from source ARN')
     .option('-f --fn <function-name>', 'Lambda Function Name')
     .action(function (arn, options) {
-        require('../lib/lambda').approve(arn, options.fn);
+        require('./lib/lambda').approve(arn, options.fn);
     });
 program
     .command('sts:assume <aws_id:role>')
     .description('Assume new role based on persmission')
     .action(function (cmd) {
-        require('../lib/sts').assume.apply(null, cmd.split(':'));
+        require('./lib/sts').assume.apply(null, cmd.split(':'));
     });
 
 if (process.stdin.isTTY) {
--- utils/request.js
+++ utils/request.js
@@ -1,40 +1,46 @@
-var req = require('https').request,
-    aws4 = require('aws4');
+'use strict';
+const req  = require('https').request,
+      aws4 = require('aws4');
 function run(opts) {
-    var defaults = run.defaultOpts || {};
-    if (!defaults.region) {
-        defaults.region = process.env.AWS_DEFAULT_REGION;
-    }
-    if (!defaults.headers) {
-        defaults.headers = { 'Content-Type': 'application/x-amz-json-1.0' };
-    }
-    //merging defaults and opts
-    opts = Object.assign(opts, run.defaultOpts);
-    if (opts.service === 'sts') {
-        delete opts.service;
-        opts.host = `sts.${opts.region}.amazonaws.com`;
-    }
-    if (opts.method) { opts.method = opts.method.toUpperCase(); }
-    if (opts.body) { opts.body = JSON.stringify(opts.body); }
-    opts = aws4.sign(opts);
-    return new Promise((resolve, reject) => {
-        req(opts, ((res) => {
-            var chunk = '';
-            res.on('data', ((c) => chunk += c));
-            res.on('end', ((_) => {
-                try {
-                    chunk = JSON.parse(chunk);
-                } catch (_err) { chunk = chunk; }
-                if (!res.statusCode.toString().match(/2\d\d/)) {
-                    return reject({ request: opts, response: chunk });
-                }
-                resolve(chunk);
-            }));
-        })).end(opts.body);
-    });
+  //EXPAND DEFAULTOPTS SO IT'S NOT OVERWRITTEN ACCESS METHOD CALLS
+  const defaults = JSON.parse(JSON.stringify(run.defaultOpts)) || {};
+
+  if (!defaults.region)  defaults.region  = process.env.AWS_DEFAULT_REGION;
+  if (!defaults.headers) defaults.headers = {'Content-Type': 'application/x-amz-json-1.0'};
+
+  //MERGING DEFAULTS AND OPTS
+  Object.assign(opts, defaults);
+
+  if (opts.service === 'sts') {
+    delete opts.service;
+    opts.host = `sts.${opts.region}.amazonaws.com`;
+  }
+
+  if (opts.body)   opts.body   = JSON.stringify(opts.body);
+  if (opts.method) opts.method = opts.method.toUpperCase();
+
+  opts = aws4.sign(opts);
+  return new Promise((resolve, reject) => {
+    req(opts, ((res) => {
+      let chunk = '';
+      res.on('data', (c) => chunk += c);
+      res.on('end', ((_) => {
+
+        try {
+          chunk = JSON.parse(chunk);
+        } catch (_err) { chunk = chunk; }
+
+        if (!res.statusCode.toString().match(/2\d\d/)) {
+          return reject({request: opts, response: chunk});
+        }
+
+        resolve(chunk);
+      }));
+    })).end(opts.body);
+  });
 }
 run.defaults = ((defaultOpts) => {
-    run.defaultOpts = defaultOpts;
-    return run;
+  run.defaultOpts = defaultOpts;
+  return run;
 });
 module.exports = run;
