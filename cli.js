f2704891a4c12a11ba53ec42c5cafda1cd9e2809
Marcus Zanona <zanona@me.com>
2016-12-06 07:31:29 -0200

    Rename --alias option due to error on commander

--- cli.js
+++ cli.js
@@ -9,14 +9,14 @@ program
     .description('Push API Gateway providing a Swagger definition')
     .option('-i --id <id>', 'The API ID')
     .action(function (definition, options) {
-        require('./lib/apigateway').push(options.id, definition);
+      require('./lib/apigateway').push(options.id, definition);
     });
 program
     .command('lambda:push [file]')
     .description('Push JS or ZIP file with AWS Lambda.')
     .description('Accepts base64 zip file as STDIN as well')
     .option('-f --fn <function-name>', 'Lambda Function Name')
-    .option('-a --alias [alias]', 'Version Alias')
+    .option('-a --al [alias]', 'Version Alias')
     .action(function (filePath, options) {
       var fileData;
       if (stdin) fileData = stdin;
@@ -37,26 +37,24 @@ program
     .description('Approve lambda invocation from source ARN')
     .option('-f --fn <function-name>', 'Lambda Function Name')
     .action(function (arn, options) {
-        require('./lib/lambda').approve(arn, options.fn);
+      require('./lib/lambda').approve(arn, options.fn);
     });
 program
     .command('sts:assume <aws_id:role>')
     .description('Assume new role based on persmission')
     .action(function (cmd) {
-        require('./lib/sts').assume.apply(null, cmd.split(':'));
+      require('./lib/sts').assume.apply(null, cmd.split(':'));
     });
 
 if (process.stdin.isTTY) {
-    program.parse(process.argv);
+  program.parse(process.argv);
 }
 else {
-    process.stdin.on('readable', function() {
-        var chunk = this.read();
-        if (chunk !== null) {
-            stdin += chunk;
-        }
-    });
-    process.stdin.on('end', function() {
-        program.parse(process.argv);
-    });
+  process.stdin.on('readable', function() {
+    var chunk = this.read();
+    if (chunk !== null) stdin += chunk;
+  });
+  process.stdin.on('end', function() {
+    program.parse(process.argv);
+  });
 }
